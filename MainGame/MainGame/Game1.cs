b42501dd5d8b865a14aee0067b968dd31b1aa095
Seiseisei <e.maraschini@libero.it>
2015-08-04 15:45:43 +0200

    Prova alternativa tabellone!

--- MainGame/MainGame/Game1.cs
+++ MainGame/MainGame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace Quantum_Game
 {
@@ -11,7 +12,8 @@ namespace Quantum_Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        private Tabellone tabellone;
+        private Tabellone2 tabellone;
+        private Texture2D textureCaselle;
 
         public Game1()
         {
@@ -31,9 +33,29 @@ namespace Quantum_Game
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // INIZIALIZZAZIONE E RIEMPIMENTO TABELLONE 4*4
+
+            tabellone = new Tabellone2(5,3);
+            int lancio = 0;
+            
+           // riempimento a caso del tabellone
+            for (int i = 0; i < tabellone.Tessere.Capacity; i++)
+            {
+                lancio = util.Dadi();
+                if (lancio == 1) tabellone.Popola(new Vuoto());
+                else tabellone.Popola(new Settore(e_sett.Pianeta10));
+               
+            }
+
+
+            Debug.WriteLine(tabellone.Tessere.Count);
+
+
+
 
             base.Initialize();
+
+
         }
 
         /// <summary>
@@ -44,8 +66,8 @@ namespace Quantum_Game
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            Texture2D textureCaselle = Content.Load<Texture2D>("Graphica\\TileSet_prova1");
-            tabellone = new Tabellone(textureCaselle, 1, 1);
+            textureCaselle = Content.Load<Texture2D>("Graphica\\TileSet_prova1");
+  
 
             // TODO: use this.Content to load your game content here
         }
@@ -80,19 +102,90 @@ namespace Quantum_Game
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 1, 1);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 1, 4);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 1, 7);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 4, 1);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 4, 4);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 4, 7);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 7, 1);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 7, 4);
-            tabellone.DisegnaCaselleQuadrante(spriteBatch, 100, 100, 7, 7);
+            //variabili varie che uso per disegnare i rettangoli
+            int casW = 50;
+            int casH = 50;
+            int tabW = casW * tabellone.Colonne * 3;
+            int tabH = casH * tabellone.Righe * 3;
+            Rectangle target = new Rectangle();
+            target.Width = casW;
+            target.Height = casH;
+            Rectangle source = new Rectangle();
+            source.Height = 100;
+            source.Width = 100;
+            float x, y;
+            int tempX, tempY;
 
-            // TODO: Add your drawing code here
+            //queste non le ho usate ma l'algoritmo vero ovviamente dovrà tenere cvonto della risoluz dello schermo
+            int scrW = graphics.PreferredBackBufferWidth;
+            int scrH = graphics.PreferredBackBufferHeight;
+        
+
+            spriteBatch.Begin();
+
+            //loop per disegnare i settori
+            for (int sett = 0; sett < tabellone.Tessere.Count; sett++) {
+
+                // il metodo indicesettore prende l'indice e mi rimanda le coordinate relative
+                // di dove sta il settore in questione sulla plancia
+                // (0,0 = angolo in alto a sin, 1,1 = angolo in basso a dx)
+                tabellone.IndiceSettore2xy(sett, out x, out y);
+                x *= (float)tabW; y *= (float)tabH; // moltiplico queste coord relative
+                                                    // per la dimensione del tabellone che vogliamo disegnare
+
+                for (int cas = 0; cas < 9; cas++) { //nested loop per disegnare le 9 caselle del settore
+
+                    //offset (valori assoluti in pixel) delle 9 caselle
+                    tempX = (cas % 3) * casW;
+                    tempY = (cas / 3) * casH;
+
+                    // se il settore è vuoto lo disegno vuoto e pace
+                    if (tabellone.Tessere[sett].Tipo == 0) { source.X = 0; source.Y = 0; }
+                    else
+                    {
+                        switch (cas) {
+
+                            case 0:
+                            case 2:
+                            case 6:
+                            case 8:
+                                source.X = 0;
+                                source.Y = 0;
+                                break;
+                            case 1:
+                            case 3:
+                            case 5:
+                            case 7:
+                                source.X = 100;
+                                source.Y = 0;
+                                break;
+                            default:
+                                source.X = 200;
+                                source.Y = 0;
+                                break;
+                        }
+
+                    }
+
+
+
+                    target.X = tempX + (int)x;
+                    target.Y = tempY + (int)y;
+
+//
+                 // Debug.WriteLine("X Rettangolo: " + target.X + "Y Rettangolo: " + target.Y);
+                    spriteBatch.Draw(textureCaselle, target, source, Color.White);
+
+                }
+
+            }
+            spriteBatch.End();
+
+
+
+
 
             base.Draw(gameTime);
         }
---
+++ MainGame/MainGame/Tabellone2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoGame.Utilities;
+using Microsoft.Xna.Framework;
+
+namespace Quantum_Game
+{ 
+    public class Tabellone2
+    {
+        private int _righe, _colonne;
+        public int Righe { get { return _righe; } }
+        public int Colonne { get { return _colonne; } }
+        public List<Zona> Tessere;
+
+        public Tabellone2(int righe, int colonne)
+        {
+
+            _righe = righe; _colonne = colonne;
+            Tessere = new List<Zona>();
+            Tessere.Capacity = righe * colonne;
+        }
+
+        /// <summary>
+        /// inserisce un settore nel tabellone (Da sinistra a destra, dall'alto verso il basso)
+        /// Se il tabellone è già pieno genera un errore!
+        /// </summary>
+        /// <param name="sec">Zona da inserire (Vuoto o Settore)</param>
+        public void Popola (Zona sec)
+        {
+            if (this.Tessere.Count < this.Tessere.Capacity)
+            {
+               Tessere.Add(sec);
+                return;
+            }
+            throw new Exception("Inseriti troppi settori nel tabellone!");
+        }
+
+
+
+        /// <summary>
+        /// Metodo che prende come input la posizione relativa x y del mouse sul tabellone (0...1)
+        /// e restituisce l'indice del settore corrispondente (nella List Tessere)
+        /// <returns>Restituisce l'indice intero a base 0 del settore nella Lista Tessere
+        /// Se il punto non è contenuto nel tabellone restituisce un valore negativo!</returns>
+        /// </summary> 
+        public int CoordinateRelative2Sett (float xRel, float yRel)
+        {
+            if (xRel >= 1 || xRel < 0 || yRel >= 1 || yRel < 0) return -1; //restituisce -1 se il punto non è sul tabellone
+            float c = (float)_colonne; float r = (float)_righe;
+            int iX, iY;
+            iX = (int)Math.Floor((xRel * c));
+            iY = ((int)Math.Floor((yRel * r))) * _colonne;
+
+            return iX + iY;
+        }
+
+        /// <summary>
+        /// Metodo che prende come input la posizione relativa x y del mouse sul tabellone (0...1)
+        /// e restituisce la posizione della casella nel settore (secondo l'enum e_posizione:
+        /// 0 = alto a sin, 1 = alto, ecc
+        /// </summary> 
+        public e_posizione CoordinateRelative2Pos (float xRel, float yRel)
+        {     
+            float c = (float)_colonne; float r = (float)_righe;
+            int iX, iY;
+            iX = (int)(Math.Floor((xRel * c)) * 3) % 3;
+            iY = (((int)Math.Floor((yRel * r))) * _colonne * 3) % 3;
+            if (iX == 1 && iY == 1) return e_posizione.pianeta;
+            return (e_posizione) iX + iY * 3;
+        }
+
+        /// <summary>
+        /// Converte l'indice del settore (cioè la sua posizione nella lista Tessere) in valori relativi float
+        /// </summary>
+        /// <param name="Id">Indice del settore di cui si vuole conoscere la posizione nel tabellone</param>
+        /// <param name="x">parametro su cui scrivere il risultato x</param>
+        /// <param name="y">parametro su cui scrivere il risultato y</param>
+        public void IndiceSettore2xy (int Id, out float x, out float y)
+        {
+            int iX, iY;
+            iX = Id % _colonne;
+            iY = (Id / _colonne);
+            x = (float)iX / (float)_colonne;
+            y = (float)iY / (float)_righe;
+            return;
+        }
+
+        /// Converte l'indice del settore (cioè la sua posizione nella lista Tessere) in valori in pixel (relativi all'origine del tabellone)
+        /// </summary>
+        /// <param name="Id">Indice del settore di cui si vuole conoscere la posizione nel tabellone</param>
+        /// <param name="x">parametro su cui scrivere il risultato x</param>
+        /// <param name="y">parametro su cui scrivere il risultato y</param>
+        public void IndiceSettore2xy (int Id, out int x, out int y, int LarghezzaTabellonePixel, int AltezzaTabellonePixel)
+        {
+            int iX, iY;
+            iX = Id % _colonne;
+            iY = (Id / _colonne);
+            x = (iX * LarghezzaTabellonePixel)/ _colonne;
+            y = (iY *AltezzaTabellonePixel) / _righe;
+            return;
+        }
+
+    }
+
+
+}
