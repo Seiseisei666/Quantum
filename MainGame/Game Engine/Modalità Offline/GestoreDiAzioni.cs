b5c3efb744fcce22e1aa895cab56488099aa681c
Giulio Aliberti <ip_happy_99@hotmail.com>
2015-09-19 19:24:26 +0200

    gestore azioni

---
+++ MainGame/Game Engine/Modalità Offline/GestoreDiAzioni.cs
@@ -0,0 +1,50 @@
+﻿using Quantum_Game.Azioni;
+using Quantum_Game.Interfaccia;
+using System.Collections.Generic;
+
+namespace Quantum_Game
+{
+    public class GestoreDiAzioni
+    {
+        /* coda di azioni da eseguire che viene gestita da questa classe */
+        private List<Azione> azioniDaEseguire;
+
+        public GestoreDiAzioni()
+        {
+            azioniDaEseguire = new List<Azione>();
+        }
+        /* se c'è almeno una azione in coda da eseguire, viene eseguita e cancellata dalla coda*/
+        private void EseguiAzione()
+        {
+            if (azioniDaEseguire.Count > 0)
+            {
+                azioniDaEseguire[0].Start();
+                azioniDaEseguire.RemoveAt(0);
+            }
+        }
+
+        /* aggiunge una azione da eseguire alla coda */
+        public void IncodaAzione(Azione azione)
+        {
+            
+            azioniDaEseguire.Add(azione);
+        }
+
+        public void MettiAzioneInTesta(Azione azione)
+        {
+            azioniDaEseguire.Insert(0, azione);
+        }
+
+        //metodo che controlla se ci sono nuove azioni ed, eventualmente, le esegue 
+        public void Update()
+        {
+            EseguiAzione();
+        }
+
+        public int Count()
+        {
+            return azioniDaEseguire.Count;
+        }
+    }
+}
+
