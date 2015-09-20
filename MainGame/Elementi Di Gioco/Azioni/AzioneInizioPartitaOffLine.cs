f4d3099c330cc1b3f52456ccf2795b44854508db
GggiulioOo <giulio.alib@gmail.com>
2015-09-20 15:21:57 +0200

    Update AzioneInizioPartitaOffLine.cs
    aggiunti alcuni commenti

--- MainGame/Elementi Di Gioco/Azioni/AzioneInizioPartitaOffLine.cs
+++ MainGame/Elementi Di Gioco/Azioni/AzioneInizioPartitaOffLine.cs
@@ -10,6 +10,10 @@ namespace Quantum_Game.Azioni
         public AzioneInizioPartitaOffLine(Quantum quantum)
         {
             //EDIT: faccio tutto nel costruttore, tanto è una botta e via questa
+            //REPLY: OK! Se il problema è che ti sta scomodo passare quantum, questa azione necessita
+            // solo di GestoreDiAzioni e GestoreDiGiocatori per funzionare (in termini di memoria sono solo due liste, molto ragionevole).
+            //Quindi si puo' costruirla come public AzioneInizioPartitaOffLine(GestoreDiAzioni a, GestoreDiGiocatori b).
+            //Probabilmente vale anche per le altre azioni, avevo messo quantum in via provvisoria
 
             quantum.getGestoreDiAzioni().IncodaAzione(new AzioneStampaAConsole("Partita iniziata!"));
 
@@ -17,6 +21,7 @@ namespace Quantum_Game.Azioni
             {
                 b.Click += b.associatedEvent;
             }
+            //probabilmente ha senso creare un'azione di inizio turno (che verrà usata anche dal bottone passa turno)
             quantum.getGestoreDiGiocatori().getGiocatoreDiTurno().Init();
             quantum.getGestoreDiAzioni().IncodaAzione(new AzioneSelezione(quantum));
             Cleanup();
@@ -32,7 +37,8 @@ namespace Quantum_Game.Azioni
         /* A fine partita i bottoni vengono disattivati */
         protected override void Cleanup()
         {// EDIT: non ho capito, questa azione andrà distrutta subito... non possiamo dissociare gli eventi da qui, servirà un'altra azione apposita
-
+         //REPLY: si, bisognerebbe fare una AzioneFinePartita in cui mettere il codice qui sotto 
+        
             /* implemntare un if(partitaInCorso) do the following code
             foreach (Interfaccia.Bottone b in quantum.getGUI().Bottoni)
             {
