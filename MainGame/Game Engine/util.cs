a49f6c2454a7e578e29232432e5af7b3e4d4624a
Seiseisei <e.maraschini@libero.it>
2015-09-21 20:38:16 +0200

    Fixxato un bug per cui non venivano mai rollate le navi 6

--- MainGame/Game Engine/util.cs
+++ MainGame/Game Engine/util.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 namespace Quantum_Game {
-public static class util {
+    /// <summary>
+    /// Sta roba non si può vedere... è una delle prime cose che avevo fatto e fa davvero schifo
+    /// c'è qualche volontario che la schiaffa da un'altra parte?
+    /// </summary>
+    public static class util {
+        
 
-	
-	// i tiri di dado avvengono tutti a proposito dei metodi delle navi, per cui incorporo nella classe nave il RNG:
-		private static Random _dado;
+private static Random _dado;
 		
 		 static util () {
 			_dado = new Random();
@@ -13,8 +16,9 @@ public static class util {
 		public static int Dadi (int num = 1) {
 			int res = 0;
 			for (int i = 0; i < num; i++) {
-				res += _dado.Next(1,6);
+				res += _dado.Next(1,7);
 			}
+            if (res > 6) throw new Exception("res");
 			return res;
 		}
 	}}
